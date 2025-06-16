97136b8644e8f9cb3ad82ddeacb77a7072cb007f
Đức Trung <152075075+DucTrung3110@users.noreply.github.com>
2025-06-16 20:28:07 +0700

    Update auth.js

--- auth/auth.js
+++ auth/auth.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
       if (email === "langla779@gmail.com" && password === "123456") {
         alert("Đăng nhập thành công!");
-        window.location.href = "index.html";
+        window.location.href = "../index.html";
       } else {
         alert("Email hoặc mật khẩu không chính xác!");
       }
